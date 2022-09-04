2f41640c6a8575b246812d265f851388fd4cac91
ut-kr <utkarsh@cyberfear.com>
2022-09-04 13:12:10 +1000

    ✨ initialize

---
+++ scripts/part2.js
@@ -0,0 +1,29 @@
+/**
+ * Author: Utkarsh KR
+ * Target: enquire.html 
+ * Purpose: Backend to the project
+**/
+
+"use strict";
+
+function validate() {
+    //
+}
+
+function get_data() {
+    //
+}
+
+function cancel_booking() {
+    //
+}
+
+function calculate_cost() {
+    //
+}
+
+function init() {
+    //
+}
+
+window.onload = init;
