1fd9fe4b4732fee7dc0fe5ba0f7a19ca21eae33a
ut-kr <utkarsh@cyberfear.com>
2022-10-16 13:19:12 +1100

    ✨ common footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!--Page footer; contains course code/title and hyperlink to swinburne.edu.au-->
+<footer>
+    <hr>
+    <table ID="footer_table">
+        <tr>
+            <td>
+                &#169; <a href="https://www.swinburne.edu.au">Swinburne University of Technology</a>
+            </td>
+            <td class="right_intend">
+                COS10011: Creating Web Applications
+            </td>
+        </tr>
+    </table>
+</footer>
+</body>
+</html>
